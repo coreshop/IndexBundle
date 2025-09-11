cdc6947e332a0257ac01a1b4d0129ee92340d03f
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2025-09-11 17:05:48 +0200

    [Studio] refactor of the refactor i guess :D

--- Resources/assets/pimcore-studio/src/modules/icon-library/index.ts
+++ Resources/assets/pimcore-studio/src/modules/icon-library/index.ts
@@ -15,31 +15,31 @@ import { serviceIds } from '@pimcore/studio-ui-bundle/app'
 import { type IconLibrary } from '@pimcore/studio-ui-bundle/modules/icon-library'
 
 // @ts-ignore
-import filterBooleanIcon from '../assets/filter-boolean.svg?react'
+import filterBooleanIcon from '../../assets/filter-boolean.svg?react'
 // @ts-ignore
-import filterCategorySelectIcon from '../assets/filter-category-select.svg?react'
+import filterCategorySelectIcon from '../../assets/filter-category-select.svg?react'
 // @ts-ignore
-import filterCombinedIcon from '../assets/filter-combined.svg?react'
+import filterCombinedIcon from '../../assets/filter-combined.svg?react'
 // @ts-ignore
-import filterEmptyIcon from '../assets/filter-empty.svg?react'
+import filterEmptyIcon from '../../assets/filter-empty.svg?react'
 // @ts-ignore
-import filterMultiselectIcon from '../assets/filter-multiselect.svg?react'
+import filterMultiselectIcon from '../../assets/filter-multiselect.svg?react'
 // @ts-ignore
-import filterRangeIcon from '../assets/filter-range.svg?react'
+import filterRangeIcon from '../../assets/filter-range.svg?react'
 // @ts-ignore
-import filterSearchIcon from '../assets/filter-search.svg?react'
+import filterSearchIcon from '../../assets/filter-search.svg?react'
 // @ts-ignore
-import filterSelectIcon from '../assets/filter-select.svg?react'
+import filterSelectIcon from '../../assets/filter-select.svg?react'
 // @ts-ignore
-import filtersIcon from '../assets/filters.svg?react'
+import filtersIcon from '../../assets/filters.svg?react'
 // @ts-ignore
-import indexesIcon from '../assets/indexes.svg?react'
+import indexesIcon from '../../assets/indexes.svg?react'
 // @ts-ignore
-import searchIcon from '../assets/search.svg?react'
+import searchIcon from '../../assets/search.svg?react'
 // @ts-ignore
-import similaritiesFieldIcon from '../assets/similarities-field.svg?react'
+import similaritiesFieldIcon from '../../assets/similarities-field.svg?react'
 // @ts-ignore
-import similarityIcon from '../assets/similarity.svg?react'
+import similarityIcon from '../../assets/similarity.svg?react'
 
 export const IndexBundleIconModule: AbstractModule = {
   onInit(): void {
